2c6f382735727d4730ef0560737bf18cf6c4c09c
Rodrigo Fontes Cruz <rodrifontes@gmail.com>
2018-07-09 12:12:40 -0300

    Iniciando a leitura do dataSet

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,20 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-public class AGClustering {
+import br.ufs.AGC.Data;
+
+public class Main {
 
 	public static void main(String[] args) throws IOException {
 		
 		BufferedReader br = new BufferedReader(new FileReader("iris.data"));
 		String row = br.readLine();
+		List<Data> dataSet = new ArrayList<Data>();
 		while (row != null) {
+			Data data = new Data();
 			System.out.println(row);
 			row = br.readLine();
 		}
---
+++ src/br/ufs/AGC/Data.java
@@ -0,0 +1,21 @@
+package br.ufs.AGC;
+
+import java.util.List;
+
+public class Data {
+	
+	private List<Object> attributes;
+	
+	public List<Object> getAttributes() {
+		return attributes;
+	}
+
+	public void setAttributes(List<Object> attributes) {
+		this.attributes = attributes;
+	}
+
+	public double getDistance(Data destiny) {
+		return 0;
+	}
+	
+}
