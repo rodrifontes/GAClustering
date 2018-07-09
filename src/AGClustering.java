0d73fe2b6b5fbc9be67ae0b174e524f86a87d57a
Rodrigo Fontes Cruz <rodrifontes@gmail.com>
2018-07-09 10:39:32 -0300

    Adicionando leitura de arquivo com o dataset

--- src/AGClustering.java
+++ src/AGClustering.java
@@ -1,9 +1,19 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class AGClustering {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
+		BufferedReader br = new BufferedReader(new FileReader("iris.data"));
+		String row = br.readLine();
+		while (row != null) {
+			System.out.println(row);
+			row = br.readLine();
+		}
 		
+		br.close();	
 		
 	}
 
