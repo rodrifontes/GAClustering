2401e0b4f92a6d979f89ddcc871d6020e23a75bb
Rodrigo Fontes <rodrifontes@gmail.com>
2018-07-09 22:31:37 -0300

    Adicionando esqueleto AG

--- src/br/ufs/AGC/AGC.java
+++ src/br/ufs/AGC/AGC.java
@@ -1,10 +1,14 @@
 package br.ufs.AGC;
 
+import org.jgap.Chromosome;
 import org.jgap.Configuration;
 import org.jgap.FitnessFunction;
+import org.jgap.Gene;
+import org.jgap.Genotype;
 import org.jgap.IChromosome;
 import org.jgap.InvalidConfigurationException;
 import org.jgap.impl.DefaultConfiguration;
+import org.jgap.impl.IntegerGene;
 
 public class AGC {
 
@@ -12,13 +16,38 @@ public class AGC {
 		return new DefaultConfiguration();
 	}
 	
-	public IChromosome clustering(GeneticParameters geneticParameters) throws InvalidConfigurationException {
+	public IChromosome clustering(GeneticParameters geneticParameters, int k) throws InvalidConfigurationException {
+		
 		Configuration conf = createConfiguration();
 		
 		FitnessFunction fitnessFunction = new ClusteringFitnessFunction();
 		conf.setFitnessFunction(fitnessFunction);
 		
-		return null;
+		Gene[] sampleGenes = new Gene[k];
+		for (int i = 0; i < sampleGenes.length; i++) {
+			sampleGenes[0] = new IntegerGene(conf, 1, k);
+		}
+		
+		Chromosome sampleChromosome = new Chromosome(conf, sampleGenes);
+		
+        conf.setSampleChromosome(sampleChromosome);
+
+        // Determino o tamanho da minha população
+        conf.setPopulationSize(geneticParameters.getPopulationSize());
+
+        // Gero minha população inicial aleatoriamente
+        Genotype population = Genotype.randomInitialGenotype(conf);
+
+        IChromosome bestSolutionSoFar = null;
+        
+        for (int i = 0; i < geneticParameters.getNumberOfEvolutions(); i++) {
+			// Inicio a evolução da população, uma geração
+            population.evolve();
+            bestSolutionSoFar = population.getFittestChromosome();
+        }
+        	
+		return bestSolutionSoFar;
+		
 	}
 	
 }
---
+++ src/br/ufs/Start.java
@@ -0,0 +1,40 @@
+package br.ufs;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+
+import br.ufs.AGC.Data;
+
+public class Start {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new FileReader("iris.data"));
+		String row = br.readLine();
+		List<Data> dataSet = new ArrayList<Data>();
+		while (row != null) {
+			Data data = new Data();
+			String[] dataSplit = row.split(",");
+			for (int i = 0; i < dataSplit.length; i++) {
+				if(Pattern.matches("^\\d+$", dataSplit[i]) || Pattern.matches("^([+-]?\\d*\\.+\\d*)$", dataSplit[i]))
+					data.getAttributes().add(new Double(dataSplit[i]));
+				else
+					data.getAttributes().add(new String(dataSplit[i]));
+			}
+			dataSet.add(data);
+			row = br.readLine();
+		}
+		
+		for (Data data : dataSet) {
+			System.out.println(data);
+		}
+		
+		br.close();	
+
+	}
+
+}
